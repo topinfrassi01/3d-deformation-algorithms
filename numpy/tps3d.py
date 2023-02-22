edec339a8b3b3ad98fdca1c3a4010cd7060b0b34
Francis Toupin <francis.toupin@hotmail.com>
2023-02-22 16:46:28 -0500

    Update tps3d.py

--- numpy/tps3d.py
+++ numpy/tps3d.py
@@ -1,36 +1,43 @@
 #coding=utf-8
 import numpy as np
 
+def compute_distance_matrix(points, clip_zero=0.0):
+    """
+    Computes a symetric matrix containing the distance between each points.
+    
+    Parameters
+    ----------
+    points : nd.array, shape [N,3]
+        Points to find the matrix for.
+     
+    Returns
+    ----------     
+    np.ndarray, shape [N,N]
+            Distance matrix between each points
+    """
+    difference = (np.expand_dims(points, axis=0) - np.expand_dims(points, axis=1))
+    return np.clip(np.sqrt(np.einsum('...k,...k->...',difference,difference)), a_min=clip_zero, a_max=None)
+
 # Translated to Python from : https://www.mathworks.com/matlabcentral/fileexchange/37576-3d-thin-plate-spline-warping-function
-def thin_plate_spline(anchors, destination, points):
+def tps_3d(points, anchors, anchors_target, anchors_weights=None):
     """
         Uses Thin-Plate Spline interpolation to deform a point cloud.
 
         Parameters
         ----------
-        anchors : nd.array
-            The anchors points are the points that guide the deformation.
-        destination : nd.array
-            These are the coordinates of the anchors after deformation. 
-            * The shape of destination and anchors should be the same and their indices should imply correspondance between points.
-        points : nd.array
+        points : np.ndarray, shape [N,3]
             The point cloud to deform.
+        anchors : np.ndarray, shape [K, 3]
+            The anchors points are the points that guide the deformation.
+        destination : np.ndarray, shape [K,3]
+            These are the coordinates of the anchors after deformation.
         Returns
         -------
-        nd.array
+        np.ndarray, shape [N,3]
             Deformed point cloud according to the anchors movement.
     """
-    n_anchors = anchors.shape[0]
-    
-    anchor_distances = np.zeros((n_anchors, n_anchors))
-
-    for row in range(n_anchors):
-        for col in range(row, n_anchors):
-            anchor_distances[row,col] = np.sum((anchors[row,:] - anchors[col,:])**2)
-            anchor_distances[col,row] = anchor_distances[row,col]
-
-    anchor_distances = np.clip(anchor_distances, a_min=1e-320, a_max=None)
-    anchor_distances = np.sqrt(anchor_distances)
+    if anchors_weights is None:
+        anchors_weights = compute_distance_matrix(anchors)
 
     P = np.hstack((np.ones((n_anchors, 1)), anchors))
     L = np.vstack( (np.hstack((anchor_distances, P)), np.hstack((np.transpose(P), np.zeros((4,4))))))
